c732ba09be26644f29811398e323a11b61196232
qiao18520 <787320213@qq.com>
2019-02-28 01:22:47 +0800

    sql

---
+++ miaosha/miaosha.sql
@@ -0,0 +1,142 @@
+/*
+SQLyog 企业版 - MySQL GUI v8.14 
+MySQL - 5.5.27 : Database - miaosha
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`miaosha` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */;
+
+USE `miaosha`;
+
+/*Table structure for table `item` */
+
+DROP TABLE IF EXISTS `item`;
+
+CREATE TABLE `item` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
+  `price` double(10,0) NOT NULL DEFAULT '0',
+  `description` varchar(500) COLLATE utf8_unicode_ci NOT NULL,
+  `sales` int(11) NOT NULL DEFAULT '0',
+  `img_url` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `item` */
+
+insert  into `item`(`id`,`title`,`price`,`description`,`sales`,`img_url`) values (6,'iphone918',222,'目前最好用的手机',226,'https://p.ssl.qhimg.com/t01138e5aba54ac6524.jpg'),(7,'iphone918',222,'目前最好用的手机',0,'https://p.ssl.qhimg.com/t01138e5aba54ac6524.jpg');
+
+/*Table structure for table `item_stock` */
+
+DROP TABLE IF EXISTS `item_stock`;
+
+CREATE TABLE `item_stock` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `stock` int(11) NOT NULL,
+  `item_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `item_stock` */
+
+insert  into `item_stock`(`id`,`stock`,`item_id`) values (1,218,6),(2,222,7);
+
+/*Table structure for table `order_info` */
+
+DROP TABLE IF EXISTS `order_info`;
+
+CREATE TABLE `order_info` (
+  `id` varchar(32) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `item_id` int(11) NOT NULL,
+  `item_price` double NOT NULL DEFAULT '0',
+  `amount` int(11) NOT NULL DEFAULT '0',
+  `order_price` double NOT NULL DEFAULT '0',
+  `promo_id` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `order_info` */
+
+insert  into `order_info`(`id`,`user_id`,`item_id`,`item_price`,`amount`,`order_price`,`promo_id`) values ('2019022300000000',14,6,222,1,222,0),('2019022300000100',14,6,222,1,222,0),('2019022300000200',14,6,100,1,100,1),('2019022300000300',15,6,100,1,100,1);
+
+/*Table structure for table `promo` */
+
+DROP TABLE IF EXISTS `promo`;
+
+CREATE TABLE `promo` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `promo_name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `start_date` datetime NOT NULL,
+  `item_id` int(11) NOT NULL,
+  `promo_item_price` double NOT NULL,
+  `end_date` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `promo` */
+
+insert  into `promo`(`id`,`promo_name`,`start_date`,`item_id`,`promo_item_price`,`end_date`) values (1,'iphone4抢购活动','2019-02-23 20:20:00',6,100,'2019-02-28 23:55:00');
+
+/*Table structure for table `sequence_info` */
+
+DROP TABLE IF EXISTS `sequence_info`;
+
+CREATE TABLE `sequence_info` (
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `current_value` int(11) NOT NULL DEFAULT '0',
+  `step` int(11) DEFAULT '0',
+  PRIMARY KEY (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `sequence_info` */
+
+insert  into `sequence_info`(`name`,`current_value`,`step`) values ('order_info',4,1);
+
+/*Table structure for table `user_info` */
+
+DROP TABLE IF EXISTS `user_info`;
+
+CREATE TABLE `user_info` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
+  `gender` tinyint(4) NOT NULL COMMENT '//1代表男性,2代表女性',
+  `age` int(11) NOT NULL,
+  `telphone` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `register_mode` varchar(255) COLLATE utf8_unicode_ci NOT NULL COMMENT '//byphone,bywechat,byalipay',
+  `third_party_id` varchar(64) COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `telphone_unique_index` (`telphone`)
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci CHECKSUM=1 DELAY_KEY_WRITE=1 ROW_FORMAT=DYNAMIC COMMENT='//1代表男性,2代表女性';
+
+/*Data for the table `user_info` */
+
+insert  into `user_info`(`id`,`name`,`gender`,`age`,`telphone`,`register_mode`,`third_party_id`) values (14,'FQ',1,23,'15627539056','byphone',NULL),(15,'qiao',1,1,'18520123','byphone',NULL),(16,'aa',1,1,'18520123123','byphone',NULL);
+
+/*Table structure for table `user_password` */
+
+DROP TABLE IF EXISTS `user_password`;
+
+CREATE TABLE `user_password` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `encrpt_password` varchar(128) COLLATE utf8_unicode_ci NOT NULL,
+  `user_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `user_password` */
+
+insert  into `user_password`(`id`,`encrpt_password`,`user_id`) values (5,'Jc5ybiPuXNGlG0DF1agYAw==',14),(6,'Jc5ybiPuXNGlG0DF1agYAw==',15),(7,'xMpCOKC5I4INzFCab3WEmw==',16);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
